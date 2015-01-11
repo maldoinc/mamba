7855d1b42c582597d23f1e376b57acb7635c2eaf
Aldo <aldo.mateli@gmail.com>
2015-01-11 22:19:32 +0100

    no moar inline scripts

--- zed.py
+++ zed.py
@@ -2,39 +2,6 @@ import interpreter.parser as p
 import interpreter.ast
 import pprint
 
-# script = '''
-#     a = 5;
-#     b = 6;
-#
-#     c= 2 - 2 * 3 + 2 - 2 * 3 + 2 - 2 * 3 + 2 - 2 * 3;
-#
-#     d = "wello" + " " + "world";
-#
-#     a = true;
-#
-#     a = 1 == 2;
-#     a = true or false;
-#
-#     b = b + 1;
-# '''
-
-
-# script = '''
-#     x = 10;
-#
-#     if x == 10 {
-#         x = 11;
-#
-#         if x == 11 {
-#             this = "works";
-#         }
-#     } else {
-#         nope = ":(";
-#     }
-#
-#     say x+x;
-# '''
-
 with open("scripts/second.sc") as f:
     script = f.read()
 
